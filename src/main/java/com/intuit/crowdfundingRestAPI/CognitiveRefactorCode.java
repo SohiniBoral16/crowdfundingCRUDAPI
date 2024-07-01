de48fc6f92d0be6cbf106d6b482871f9cec5b69a
SohiniBoral16 <124071086+SohiniBoral16@users.noreply.github.com>
2024-07-01 12:24:07 +0530

    Update CognitiveRefactorCode.java

--- src/main/java/com/intuit/crowdfundingRestAPI/CognitiveRefactorCode.java
+++ src/main/java/com/intuit/crowdfundingRestAPI/CognitiveRefactorCode.java
@@ -1,3 +1,59 @@
+// Load versions from the properties file
+def versions = new Properties()
+file('versions.properties').withInputStream { stream ->
+    versions.load(stream)
+}
+
+buildscript {
+    dependencies {
+        classpath "org.jsonschema2pojo:jsonschema2pojo-gradle-plugin:1.1.0"
+    }
+}
+
+plugins {
+    id 'application'
+    id 'org.springframework.boot' version "${versions['springBootVersion']}"
+    id 'io.spring.dependency-management' version "${versions['dependencyManagementVersion']}"
+    id 'java'
+    id 'com.ms.gradle.train-metadata' version "${versions['trainMetadataVersion']}"
+    id 'com.ms.gradle.afs-install' version "${versions['afsInstallVersion']}"
+    id 'com.ms.gradle.runscripts' version "${versions['runScriptsVersion']}"
+    id 'jacoco'
+    id 'org.barfuin.gradle.jacocolog' version "${versions['jacocoLogVersion']}"
+    id 'com.google.cloud.tools.jib' version "${versions['jibVersion']}"
+}
+
+jacoco {
+    toolVersion = "${versions['jacocoVersion']}"
+}
+
+group = 'com.ms.clientData'
+version = '1.0'
+sourceCompatibility = "${versions['javaVersion']}"
+apply from: file("${rootProject.projectDir}/docker.gradle")
+
+ext {
+    set('springCloudVersion', "${versions['springCloudVersion']}")
+    imagePackageDir = "./packages/docker/${account}/${application}/${version}/"
+    trainDir = "${imagePackageDir}/metadata/"
+}
+
+sourceSets {
+    integrationTest {
+        java.srcDirs = ['src/integrationTest/java']
+        resources.srcDirs = ['src/integrationTest/resources']
+        compileClasspath += main.output + test.output
+        runtimeClasspath += main.output + test.output
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom annotationProcessor
+    }
+}
+
+
 springBootVersion=2.5.13
 dependencyManagementVersion=1.0.11.RELEASE
 javaVersion=11
