107e2b0e74b17a821ce7e9a365a628fe49df30ad
SohiniBoral16 <124071086+SohiniBoral16@users.noreply.github.com>
2024-07-01 11:58:37 +0530

    Update CognitiveRefactorCode.java

--- src/main/java/com/intuit/crowdfundingRestAPI/CognitiveRefactorCode.java
+++ src/main/java/com/intuit/crowdfundingRestAPI/CognitiveRefactorCode.java
@@ -1,23 +1,63 @@
-plugins {
-    id 'org.springframework.boot' version '2.5.13'
-    id 'io.spring.dependency-management' version '1.0.11.RELEASE'
-    id 'java'
-    id 'jacoco'
-    id 'org.sonarqube' version '4.0.0.2929' // Add this line to apply the SonarQube plugin
+springBootVersion=2.5.13
+dependencyManagementVersion=1.0.11.RELEASE
+javaVersion=11
+trainMetadataVersion=1.0.0
+afsInstallVersion=1.0.0
+runScriptsVersion=1.0.0
+jacocoVersion=0.8.11
+jacocoLogVersion=3.1.0
+jibVersion=1.0.0
+springCloudVersion=2020.0.2
+
+// Load versions from the properties file
+def versions = new Properties()
+file('versions.properties').withInputStream { versions.load(it) }
+
+buildscript {
+    dependencies {
+        classpath "org.jsonschema2pojo:jsonschema2pojo-gradle-plugin:1.1.0"
+    }
 }
 
+plugins {
+    id 'application'
+    id 'org.springframework.boot' version "${versions.springBootVersion}"
+    id 'io.spring.dependency-management' version "${versions.dependencyManagementVersion}"
+    id 'java'
+    id 'com.ms.gradle.train-metadata' version "${versions.trainMetadataVersion}"
+    id 'com.ms.gradle.afs-install' version "${versions.afsInstallVersion}"
+    id 'com.ms.gradle.runscripts' version "${versions.runScriptsVersion}"
+    id 'jacoco'
+    id 'org.barfuin.gradle.jacocolog' version "${versions.jacocoLogVersion}"
+    id 'com.google.cloud.tools.jib' version "${versions.jibVersion}"
+}
 
-sonarqube {
-    properties {
-        property "sonar.projectKey", "your.project.key"
-        property "sonar.projectName", "Your Project Name"
-        property "sonar.projectVersion", "1.0"
-        property "sonar.sourceEncoding", "UTF-8"
-        property "sonar.sources", "src/main/java"
-        property "sonar.tests", "src/test/java"
-        property "sonar.java.binaries", "build/classes/java/main"
-        property "sonar.junit.reportPaths", "build/test-results/test"
-        property "sonar.jacoco.reportPaths", "build/jacoco/test.exec"
-        // Add any other necessary properties here
+jacoco {
+    toolVersion = "${versions.jacocoVersion}"
+}
+
+group = 'com.ms.clientData'
+version = '1.0'
+sourceCompatibility = "${versions.javaVersion}"
+apply from: file("${rootProject.projectDir}/docker.gradle")
+
+ext {
+    set('springCloudVersion', "${versions.springCloudVersion}")
+    imagePackageDir = "./packages/docker/${account}/${application}/${version}/"
+    trainDir = "${imagePackageDir}/metadata/"
+}
+
+sourceSets {
+    integrationTest {
+        java.srcDirs = ['src/integrationTest/java']
+        resources.srcDirs = ['src/integrationTest/resources']
+        compileClasspath += main.output + test.output
+        runtimeClasspath += main.output + test.output
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom annotationProcessor
     }
 }
