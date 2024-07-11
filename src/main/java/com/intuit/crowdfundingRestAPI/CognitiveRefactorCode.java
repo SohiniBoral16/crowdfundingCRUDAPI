fe7e125805c90d819733fb2a044f4aedbfdf5ad7
SohiniBoral16 <124071086+SohiniBoral16@users.noreply.github.com>
2024-07-11 14:59:11 +0530

    Update CognitiveRefactorCode.java

--- src/main/java/com/intuit/crowdfundingRestAPI/CognitiveRefactorCode.java
+++ src/main/java/com/intuit/crowdfundingRestAPI/CognitiveRefactorCode.java
@@ -1,4 +1,47 @@
 
+plugins {
+    id 'org.springframework.boot' version "${springBootVersion}"
+    id 'io.spring.dependency-management' version "${dependencyManagementVersion}"
+    id 'java'
+    id 'jacoco'
+    id 'com.gradle.plugin-publish' version '0.10.1'
+    id 'com.bmuschko.docker-java-application' version '6.6.1'
+    id 'org.sonarqube' version '4.3.0.3225' // Adding SonarQube plugin
+    id 'com.google.cloud.tools.jib' version '3.1.4'
+}
+
+sonarqube {
+    properties {
+        property "sonar.projectKey", "clientData/pipex"
+        property "sonar.projectName", "Pipex"
+        property "sonar.language", "java"
+        property "sonar.scm.provider", "git"
+        property "sonar.instance.name", "sonar-prod:sonarprod"
+        property "sonar.host.url", "https://sonarqube.example.com" // Updated to remote server URL
+        property "sonar.login", "yourAuthenticationToken" // Ensure you have the correct authentication token if required
+        property "sonar.java.libraries", "/ms/dist/oss/java/PROJ/lombok/1.18.10/lib/*.jar"
+        
+        property "sonar.sources", "src/main/java"
+        property "sonar.tests", "src/test/java"
+        property "sonar.java.libraries", "build/lib/p2pservice*.jar"
+        property "sonar.java.binaries", "build/classes"
+        property "sonar.junit.reportPaths", "build/test-results/test"
+        property "sonar.jacoco.reportPaths", "build/jacoco/test.exec"
+        property "sonar.coveragePlugin", "jacoco"
+        property "sonar.exclusions", "**/generated/**,**/config/**,**/enum/**,**/repository/**,**/util/**,**/security/**"
+        property "sonar.inclusions", "**/constants/**,**/P2PServiceApplication.java"
+        property "sonar.scm.provider", "git"
+    }
+}
+
+// Ensure the analysis task runs before the build task
+tasks.build {
+    dependsOn "sonarqube"
+}
+
+
+-----------------------------------
+     
      plugins {
     id 'org.springframework.boot' version "${springBootVersion}"
     id 'io.spring.dependency-management' version "${dependencyManagementVersion}"
