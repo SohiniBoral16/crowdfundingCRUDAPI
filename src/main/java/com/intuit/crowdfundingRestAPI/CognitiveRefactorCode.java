85751bd575b4d152e5fc29f94dd1513e253e2c1b
SohiniBoral16 <124071086+SohiniBoral16@users.noreply.github.com>
2024-07-16 16:56:29 +0530

    Update CognitiveRefactorCode.java

--- src/main/java/com/intuit/crowdfundingRestAPI/CognitiveRefactorCode.java
+++ src/main/java/com/intuit/crowdfundingRestAPI/CognitiveRefactorCode.java
@@ -8,9 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(RelationshipVisualizationController.class)
+@WebMvcTest(controllers = RelationshipVisualizationController.class,
+            excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebstackSecurityConfig.class))
 public class RelationshipVisualizationControllerTest {
 
     @Autowired
