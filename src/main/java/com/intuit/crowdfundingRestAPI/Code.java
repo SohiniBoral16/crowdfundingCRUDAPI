c2a7690ee07ab6d8cbf2b06b6b75817467ce5236
SohiniBoral16 <124071086+SohiniBoral16@users.noreply.github.com>
2024-08-22 19:44:09 +0530

    Update Code.java

--- src/main/java/com/intuit/crowdfundingRestAPI/Code.java
+++ src/main/java/com/intuit/crowdfundingRestAPI/Code.java
@@ -1,3 +1,33 @@
+
+
+private List<TargetParty> fetchTargetParty(List<String> targetPartyIds) {
+    var targetPartiesData = getPartiesFromCoda(targetPartyIds, TARGET_PARTY_DOM_ATTRIBUTES);
+    List<TargetParty> targetParties = new ArrayList<>();
+
+    for (Party targetPartyData : targetPartiesData) {
+        TargetParty targetParty = TargetParty.builder()
+            .targetPartyId(targetPartyData.getPartyID())
+            .targetPartyRelatedParties(
+                targetPartyData.getRelatedPartyList().stream()
+                    .map(relatedParty -> TargetPartyRelatedParties.builder()
+                        .relatedPartyId(relatedParty.getRole1Party().getPartyID())
+                        .relationshipTypeId(
+                            Collections.singletonList(relatedParty.getPartyRelationshipType().getID())
+                        )
+                        .build()
+                    )
+                    .collect(Collectors.toList())
+            )
+            .build();
+
+        targetParties.add(targetParty);
+    }
+
+    log.info("Target party details: {}", targetParties);
+    return targetParties;
+}
+
+
 private List<TargetParty> fetchTargetParty(List<String> targetPartyIds) {
     var targetPartiesData = getPartiesFromCoda(targetPartyIds, TARGET_PARTY_DOM_ATTRIBUTES);
     List<TargetParty> targetParties = new ArrayList<>();
